7abdc77413a8f7972487f6caf1140d6bd0dec001
Jaap Coomans <jaapcoomans@gmail.com>
2025-03-18 20:59:21 +0100

    Add test cases for unhappy flows and add hibernate validator

--- player-profile-service/src/test/java/nl/jaapcoomans/demo/playerprofile/PlayerProfileApiTest.java
+++ player-profile-service/src/test/java/nl/jaapcoomans/demo/playerprofile/PlayerProfileApiTest.java
@@ -7,6 +7,7 @@ import nl.jaapcoomans.demo.playerprofile.domain.PlayerProfileService;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static nl.jaapcoomans.demo.playerprofile.PlayerProfileTestDataFactory.aPlayerId;
 import static nl.jaapcoomans.demo.playerprofile.PlayerProfileTestDataFactory.aPlayerProfile;
 import static org.mockito.Mockito.when;
 
@@ -18,7 +19,7 @@ public class PlayerProfileApiTest {
     private static final OpenApiContract apiContract = OpenApiContract.fromClasspath("openapi/player-profile-api.json");
 
     @Test
-    public void test() {
+    public void getPlayerProfile() {
         // Given
         var profile = aPlayerProfile();
         var playerId = profile.playerId();
@@ -37,9 +38,36 @@ public class PlayerProfileApiTest {
         var body = response.body().asString();
         apiContract.assertThat(body).isValidResponseFor("getPlayerProfile", 200);
     }
-    
+
+    @Test
+    public void getPlayerProfileNotFound() {
+        // Given
+        var playerId = aPlayerId();
+        when(playerProfileService.getPlayerProfile(playerId))
+                .thenReturn(null);
+
+        // When
+        var response = given()
+                .get("/players/{playerId}", playerId);
+
+        // Then
+        response.then()
+                .statusCode(404);
+        var body = response.body().asString();
+        apiContract.assertThat(body).isValidResponseFor("getPlayerProfile", 404);
+    }
+
     @Test
     public void testInvalidPlayerId() {
+        // Given
+        var playerId = "this-is-an-invalid-player-id";
+
+        // When
+        var response = given()
+                .get("/players/{playerId}", playerId);
         
+        // Then
+        response.then()
+                .statusCode(400);
     }
 }
